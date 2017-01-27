879cb9a57fe2d4f8c9472d4b63f321a3eaa78031
Clark Kent <superman@kryptonite.planet>
2017-01-27 04:20:52 -0600

    updates for 2k card recognition

--- miLazyCracker.sh
+++ miLazyCracker.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+# This is a general-purpose function to ask Yes/No questions in Bash, either
+# with or without a default answer. It keeps repeating the question until it
+# gets a valid answer.
+
+ask() {
+    # http://djm.me/ask
+    local prompt default REPLY
+
+    while true; do
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read REPLY </dev/tty
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
 myUID=$(nfc-list -t 1|sed -n 's/ //g;/UID/s/.*://p')
 TMPFILE_MFD="mfc_${myUID}_dump.mfd"
 TMPFILE_UNK="mfc_${myUID}_unknownMfocSectorInfo.txt"
@@ -74,6 +115,9 @@ done
 rm -f "$TMPFILE_UNK" "$TMPFILE_FND" "0x${myUID}_"*".txt"
 if [ $mfocResult -eq 0 ]; then
     echo -e "\n\nDump left in: $TMPFILE_MFD"
+    if ask "Do you want clone the card? Place card on reader now and press Y"; then
+         nfc-mfclassic W a $TMPFILE_MFD
+    fi
 else
     rm -f "$TMPFILE_MFD"
 fi
